38ffc2970e876af39125d8d66f5634e67c2e69d2
Søren Sønderby <tropehjelm@gmail.com>
2014-08-14 13:42:08 +0200

    update example

--- test-mnist-discriminative.lua
+++ test-mnist-discriminative.lua
@@ -7,18 +7,18 @@ require 'paths'
 torch.manualSeed(101)
 torch.setdefaulttensortype('torch.FloatTensor')
 
-
+-- LOAD DATA
 mnist_folder = '../mnist-th7'
 rescale = 1
-mnist.createdatasets(mnist_folder,rescale)     
---------------------------
---       SETUP RBM      --
---------------------------
+x_train, y_train, x_val, y_val, x_test, y_test = mnist.createdatasets(mnist_folder,rescale) 
+   
+
+-- SETUP RBM
 local opts = {}
-tempfile = 'discriminative_temp.asc'
-tempfolder = '../rbmtemp'
+local tempfile = 'discriminative_temp.asc'
+local tempfolder = '../rbmtemp'
 os.execute('mkdir -p ' .. tempfolder)              -- create tempfolder if it does not exist
-finalfile = 'discriminative_final.asc'             -- Name of final RBM file
+local finalfile = 'discriminative_final.asc'             -- Name of final RBM file
 os.execute('mkdir -p ' .. tempfolder)              -- Create save folder if it does not exists
 opts.tempfile = paths.concat(tempfolder,tempfile)  -- current best is saved to this folder
 opts.n_hidden     = 500
@@ -29,13 +29,13 @@ opts.alpha = 0
 opts.beta = 0
 opts.isgpu = 0
 
--- DO Training
+-- DO TRAINING
 local rbm = rbmsetup(opts,x_train, y_train)
 rbm = rbmtrain(rbm,x_train,y_train,x_val,y_val)
 saverbm(paths.concat(tempfolder,tempfile),rbm)
-acc_train = accuracy(rbm,x_train,y_train)
-acc_val = accuracy(rbm,x_val,y_val)
-acc_test = accuracy(rbm,x_test,y_test)
+local acc_train = accuracy(rbm,x_train,y_train)
+local acc_val = accuracy(rbm,x_val,y_val)
+local acc_test = accuracy(rbm,x_test,y_test)
 print('Train error      : ', 1-acc_train)
 print('Validation error : ', 1-acc_val)
 print('Test error       : ', 1-acc_test)
