11e354d37e849867ff6c83acb656dcb1beda0be2
Søren Sønderby <skaaesonderby@gmail.com>
2014-08-15 15:47:16 +0200

    remove save printout

--- rbm-util.lua
+++ rbm-util.lua
@@ -249,7 +249,7 @@ function saverbm(filename,rbm)
      file = torch.DiskFile(filename, 'w')
      file:writeObject(rbm)
      file:close() -- make sure the data is written
-     print("RBM saved: ", filename)
+     --print("RBM saved: ", filename)
 end
 
 function loadrbm(filename)
